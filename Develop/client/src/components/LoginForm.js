508d2f89d5c67521b3f0d56279d24a4a6da7352e
cjsolis <cjelizondo12@gmail.com>
2023-06-27 09:43:11 -0500

    imported mutation in loginform.js

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from "@apollo/client";
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [loginUser, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,7 +28,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await login(userFormData);
+      const response = await loginUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
