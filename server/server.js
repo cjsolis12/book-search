178cb184a35bb702bbc04c21c32931d354e8da77
cjsolis <cjelizondo12@gmail.com>
2023-08-06 09:41:09 -0500

    node version .env

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const { ApolloServer } = require ('apollo-server-express')
 const path = require('path');
 const db = require('./config/connection');
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const { typeDefs, resolvers } = require ('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
