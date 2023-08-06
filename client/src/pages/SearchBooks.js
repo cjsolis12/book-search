8b522e1dbe79e490a6be0298df0998ab52ce39bc
cjsolis <cjelizondo12@gmail.com>
2023-08-06 10:06:21 -0500

    removed variables not being used

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,7 +10,7 @@ const SearchBooks = () => {
   const [searchedBooks, setSearchedBooks] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-  const [saveBook, {error}]  = useMutation(SAVE_BOOK);
+  const [saveBook]  = useMutation(SAVE_BOOK);
 
 
   useEffect(() => {
